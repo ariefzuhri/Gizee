aecc0e50903c27141290652a5aed297ab326f055
ariefzuhri <ariefzuhri72@gmail.com>
2021-08-06 15:11:55 +0700

    implement certificate pinning

--- core/src/main/java/com/ariefzuhri/gizee/core/di/CoreModule.kt
+++ core/src/main/java/com/ariefzuhri/gizee/core/di/CoreModule.kt
@@ -12,6 +12,7 @@ import com.ariefzuhri.gizee.core.domain.repository.IFoodRepository
 import com.ariefzuhri.gizee.core.utils.AppExecutors
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -38,11 +39,21 @@ val databaseModule = module {
 
 val networkModule = module {
     single {
+        val hostname = "trackapi.nutritionix.com"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/fajdlzqjFkH3fU8/NrjW0d4cFANUzh/4HstyvlVaTqM=")
+            .add(hostname, "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+            .add(hostname, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .add(hostname, "sha256/KwccWaCgrnaw6tsrrSO61FgLacNgG2MMLq8GE6+oP5I=")
+            .build()
         val httpClient = OkHttpClient.Builder()
         if (BuildConfig.DEBUG) {
-            httpClient.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-            httpClient.connectTimeout(120, TimeUnit.SECONDS)
-            httpClient.readTimeout(120, TimeUnit.SECONDS)
+            with(httpClient) {
+                addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                connectTimeout(120, TimeUnit.SECONDS)
+                readTimeout(120, TimeUnit.SECONDS)
+                certificatePinner(certificatePinner)
+            }
         }
         httpClient.build()
     }
