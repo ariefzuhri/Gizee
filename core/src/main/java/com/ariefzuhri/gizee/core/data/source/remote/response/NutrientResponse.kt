b6baf49b472a1cbdc04643005a351dfda32132b2
ariefzuhri <ariefzuhri72@gmail.com>
2021-08-07 17:44:52 +0700

    Remove unused response attributes

--- core/src/main/java/com/ariefzuhri/gizee/core/data/source/remote/response/NutrientResponse.kt
+++ core/src/main/java/com/ariefzuhri/gizee/core/data/source/remote/response/NutrientResponse.kt
@@ -6,27 +6,12 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class NutrientResponse(
 
+    @field:Json(name = "attr_id")
+    val attrId: Int? = null,
+
     @field:Json(name = "unit")
     val unit: String? = null,
 
-    @field:Json(name = "api_name")
-    val apiName: String? = null,
-
-    @field:Json(name = "usda_tag")
-    val usdaTag: String? = null,
-
-    @field:Json(name = "name")
-    val name: String? = null,
-
-    @field:Json(name = "fda_daily_value")
-    val fdaDailyValue: Int? = null,
-
     @field:Json(name = "usda_nutr_desc")
-    val usdaNutrDesc: String? = null,
-
-    @field:Json(name = "usda_sr_order")
-    val usdaSrOrder: Int? = null,
-
-    @field:Json(name = "attr_id")
-    val attrId: Int? = null
+    val usdaNutrDesc: String? = null
 )
