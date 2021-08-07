9fc705cbab52b706ffc68436d882b4a057b4f975
ariefzuhri <ariefzuhri72@gmail.com>
2021-08-07 09:55:16 +0700

    Replace Log calls to Timber

--- app/src/main/java/com/ariefzuhri/gizee/MyApplication.kt
+++ app/src/main/java/com/ariefzuhri/gizee/MyApplication.kt
@@ -6,17 +6,24 @@ import android.app.Application
 import com.ariefzuhri.gizee.core.di.databaseModule
 import com.ariefzuhri.gizee.core.di.networkModule
 import com.ariefzuhri.gizee.core.di.repositoryModule
+import com.ariefzuhri.gizee.core.utils.ReleaseTree
 import com.ariefzuhri.gizee.di.useCaseModule
 import com.ariefzuhri.gizee.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
+import timber.log.Timber
 
 class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        initKoin()
+        initTimber()
+    }
+
+    private fun initKoin() {
         startKoin {
             androidLogger(Level.NONE)
             androidContext(this@MyApplication)
@@ -29,6 +36,15 @@ class MyApplication : Application() {
                     viewModelModule
                 )
             )
+        }
+
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
+            Timber.plant(ReleaseTree())
         }
     }
 }
