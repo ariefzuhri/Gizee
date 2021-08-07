165716845f6a04d2a4f261206acba854791897e5
ariefzuhri <ariefzuhri72@gmail.com>
2021-08-07 09:30:22 +0700

    Add no network connection error message when displaying search result

--- core/src/main/java/com/ariefzuhri/gizee/core/utils/AppUtils.kt
+++ core/src/main/java/com/ariefzuhri/gizee/core/utils/AppUtils.kt
@@ -2,12 +2,27 @@ package com.ariefzuhri.gizee.core.utils
 
 import android.content.Context
 import android.widget.Toast
+import java.io.IOException
 import java.text.DecimalFormat
 
 object AppUtils {
 
     fun formatToDecimal(number: Number?): String {
         return DecimalFormat("0.#").format(number ?: 0)
+    }
+
+    fun isNetworkAvailable(): Boolean {
+        val runtime = Runtime.getRuntime()
+        try {
+            val ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8")
+            val exitValue = ipProcess.waitFor()
+            return exitValue == 0
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+        return false
     }
 
     fun showToast(context: Context?, message: CharSequence?) {
