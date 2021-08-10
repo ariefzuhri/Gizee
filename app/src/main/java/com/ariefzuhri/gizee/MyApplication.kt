e04b75ce062514d92f17c9e0f3c8279dccfdf1da
ariefzuhri <ariefzuhri72@gmail.com>
2021-08-10 08:37:46 +0700

    Add Logger

--- app/src/main/java/com/ariefzuhri/gizee/MyApplication.kt
+++ app/src/main/java/com/ariefzuhri/gizee/MyApplication.kt
@@ -1,26 +1,41 @@
-@file:Suppress("unused")
-
 package com.ariefzuhri.gizee
 
 import android.app.Application
+import android.util.Log
 import com.ariefzuhri.gizee.core.di.databaseModule
 import com.ariefzuhri.gizee.core.di.networkModule
 import com.ariefzuhri.gizee.core.di.repositoryModule
-import com.ariefzuhri.gizee.core.utils.ReleaseTree
 import com.ariefzuhri.gizee.di.useCaseModule
 import com.ariefzuhri.gizee.di.viewModelModule
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.orhanobut.logger.Logger
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 import timber.log.Timber
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.PrettyFormatStrategy
 
+@Suppress("unused")
 class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         initKoin()
+        initLogger()
         initTimber()
+    }
+
+    private fun initLogger() {
+        val formatStrategy = PrettyFormatStrategy.newBuilder()
+            .tag("DEV_LOG")
+            .build()
+        Logger.addLogAdapter(object : AndroidLogAdapter(formatStrategy) {
+            override fun isLoggable(priority: Int, tag: String?): Boolean {
+                return BuildConfig.DEBUG
+            }
+        })
     }
 
     private fun initKoin() {
@@ -41,9 +56,27 @@ class MyApplication : Application() {
 
     private fun initTimber() {
         if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
+            Timber.plant(DebugTree())
         } else {
             Timber.plant(ReleaseTree())
+        }
+    }
+
+    private class DebugTree : Timber.DebugTree() {
+        override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+            Logger.log(priority, tag, message, t)
+        }
+    }
+
+    private class ReleaseTree : Timber.Tree() {
+        override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+            if (priority == Log.ERROR || priority == Log.WARN) {
+                if (t == null) {
+                    FirebaseCrashlytics.getInstance().log(message)
+                } else {
+                    FirebaseCrashlytics.getInstance().recordException(t)
+                }
+            }
         }
     }
 }
